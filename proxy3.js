07740ea4495412248c5d750604b18a164a603148
Steve Louis <canjoe2233@gmail.com>
2016-11-30 00:25:22 -0500

    Proxying to Google

--- proxy3.js
+++ proxy3.js
@@ -1,27 +1,56 @@
+var http = require('http');
+var https = require('https');
 var net = require('net');
 var url = require('url');
 
 var LOCAL_PORT  = 8080;
-var REMOTE_PORT = 8080;
-var REMOTE_ADDR = "smartvue.com";
+var REMOTE_PORT = 443;
+var REMOTE_HOST = "google.com";
+const server = http.createServer( (c_req, c_res) => {
+    console.log('Receiving Connection!!!');
+    client_headers = c_req.headers;
+    client_method = c_req.method;
+    client_url = c_req.url;
 
-var server = net.createServer(function (socket) {
-    //var request_url = url.parse(request.url);
-    socket.on('data', function (msg) {
-        console.log('  ** START **');
-        console.log('<< From client to proxy ', msg.toString());
-        var serviceSocket = new net.Socket();
-        serviceSocket.connect(parseInt(REMOTE_PORT), REMOTE_ADDR, function () {
-            console.log('>> From proxy to remote', msg.toString());
-            serviceSocket.write(msg);
-        });
-        serviceSocket.on("data", function (data) {
-            console.log('<< From remote to proxy', data.toString());
-            socket.write(data);
-            console.log('>> From proxy to client', data.toString());
-        });
+    // Update proxy headers
+    client_headers['host'] = REMOTE_HOST;
+
+    // Proxy http request to Google
+    options = {
+        hostname: REMOTE_HOST,
+        path: client_url,
+        method: client_method,
+        headers: client_headers
+    };
+    p_req = https.request( options, (p_res) => {
+            console.log('Connecting to proxy');
+            console.log('Proxy status code: ', p_res.statusCode);
+            if ( p_res.statusCode == 301 || p_res.statusCode == 307 ){
+                // Get new hostname
+                options['hostname']  = p_res.headers['location'];
+                console.log('Options: ', options);
+                
+                p_redirect_req = https.request( options, (p_redirect_res) => {
+                    console.log('Inside the magic');
+                    p_redirect_res.on('data', (data) => {
+                        c_res.writeHead(200, {'X-Super-Proxy': 'Proxying'});
+                        c_res.end(data);
+                    })
+                });
+                p_redirect_req.end();
+            }
+            else {
+                p_res.on('data', (data) => {
+                    c_res.writeHead(200, {'X-Super-Proxy': 'Proxying'});
+                    c_res.end(data);
+                });
+            }
+        }
+    );
+    p_req.end();
+    p_req.on('error', (err) => {
+        console.error(err);
     });
 });
 
-server.listen(LOCAL_PORT);
-console.log("TCP server accepting connection on port: " + LOCAL_PORT);
+server.listen(8080);
