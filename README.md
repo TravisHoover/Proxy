0657e9945839219f5ca49cd337267a6bde95864e
TravisHoover <traviskhoover@gmail.com>
2016-12-01 21:21:59 -0600

    Stripped out projects not pursued. Updated README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Proxy
+# Proxy server
+**Maintainer: Travis Hoover**
+
+This is a standalone proxy server using the Node.js framework
+
+## Prerequisites
+Install Node.js (nodejs.org)
+
+```bash
+$ npm install
+```
+
+
+## Running the server
+```bash
+$ node proxy.js
+```
--- proxy.js
+++ proxy.js
@@ -1,55 +1,54 @@
-/*jshint node:true*/
-'use strict';
- 
+var http = require('http');
+var https = require('https');
 var net = require('net');
- 
-var proxyPort = 9020;
-var tcpServerPort = 9030;
- 
-// proxy server
-var proxy = net.createServer(function (socket) {
-    var client;
- 
-    console.log('Client connected to proxy');
- 
-    // Create a new connection to the TCP server
-    client = net.connect(tcpServerPort);
- 
-    // 2-way pipe between client and TCP server
-    socket.pipe(client).pipe(socket);
+var url = require('url');
 
-    server.listen(9020, function () {
-        var port = server.address().port;
-        console.log('HTTP(s) proxy server listening on port %d', port);
-    });
+var REMOTE_HOST = "google.com";
+const server = http.createServer( (c_req, c_res) => {
+    console.log('Receiving Connection!!!');
+    client_headers = c_req.headers;
+    client_method = c_req.method;
+    client_url = c_req.url;
 
+    // Update proxy headers
+    client_headers['host'] = REMOTE_HOST;
 
-    socket.on('close', function () {
-        console.log('Client disconnected from proxy');
-    });
- 
-    socket.on('error', function (err) {
-        console.log('Error: ' + err.soString());
-    });
-});
- 
-// a simple TCP server for testing
-var server = net.createServer(function (socket) {
-    console.log('Client connected to server');
- 
-    socket.on('close', function () {
-        console.log('Client disconnected from server');
-    });
- 
-    socket.on('data', function (buffer) {
-        // 'echo' server
-        socket.write(buffer);
-    });
- 
-    socket.on('error', function (err) {
-        console.log('Error: ' + err.soString());
-    });
-});
- 
-server.listen(tcpServerPort);
-proxy.listen(proxyPort);
+    // Proxy http request to Google
+    options = {
+        hostname: REMOTE_HOST,
+        path: client_url,
+        method: client_method,
+        headers: client_headers
+    };
+    p_req = https.request( options, (p_res) => {
+            console.log('Connecting to proxy');
+            console.log('Proxy status code: ', p_res.statusCode);
+            if ( p_res.statusCode == 301 || p_res.statusCode == 307 ){
+                // Get new hostname
+                options['hostname']  = p_res.headers['location'];
+                console.log('Options: ', options);
+                
+                p_redirect_req = https.request( options, (p_redirect_res) => {
+                    console.log('Inside the magic');
+                    p_redirect_res.on('data', (data) => {
+                        c_res.writeHead(200, {'X-Super-Proxy': 'Proxying'});
+                        c_res.end(data);
+                    })
+                })
+                p_redirect_req.end();
+            }
+            else {
+                p_res.on('data', (data) => {
+                    c_res.writeHead(200, {'X-Super-Proxy': 'Proxying'});
+                    c_res.end(data);
+                })
+            }
+        }
+    )
+    p_req.end();
+    p_req.on('error', (err) => {
+        console.error(err);
+    })
+})
+
+server.listen(8080);
